43abff5475c78da6cc255cc9181f80fa43e611be
antinucleus <antinucleuss@gmail.com>
2024-05-26 19:28:12 +0300

    fix: change svg file dir

--- imagetosvg.sh
+++ imagetosvg.sh
@@ -1,5 +1,5 @@
 filename=$1
 extension=${filename##*.}
 convert ${filename} -canny 0x1+10%+30% -negate out.${extension}
-convert out.${extension} ./public/result.svg
+convert out.${extension} /home/oxygen/Desktop/PlotterController/plotter-express-server/public/result.svg
 rm out.${extension}
